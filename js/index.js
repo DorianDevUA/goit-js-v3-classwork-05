9a42ef4500fed9f144fe419c0267f8da887b4b89
Dorian Devua <Dorian.DevUa@gmail.com>
2024-09-16 00:26:13 +0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,174 @@
+// =====================
+// Repeta ==============
+// =====================
+
+/*
+ * =============================================
+ * =============== Shoping Cart ================
+ * =============================================
+ * Работем с коллекцией товаров в корзине:
+ * - getItems()
+ * - add(product)
+ * - remove(productName)
+ * - clear()
+ * - countTotalPrice()
+ * - increaseQuantity(productName)
+ * - decreaseQuantity(productName)
+ *
+ * { name: '🍎', price: 50 }
+ * { name: '🍇', price: 70 }
+ * { name: '🍋', price: 60 }
+ * { name: '🍓', price: 110 }
+ */
+
+const cart = {
+  items: [],
+  getItems() {},
+  add(product) {},
+  remove(productName) {},
+  clear() {},
+  countTotalPrice() {},
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
+
+// console.table(cart.getItems());
+
+cart.add({ name: '🍎', price: 50 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍋', price: 60 });
+cart.add({ name: '🍓', price: 110 });
+
+// console.table(cart.getItems());
+
+cart.remove('🍎');
+// console.table(cart.getItems());
+
+// cart.clear();
+// console.table(cart.getItems());
+
+// cart.increaseQuantity('🍎');
+// console.table(cart.getItems());
+
+// cart.decreaseQuantity('🍋');
+// cart.decreaseQuantity('🍋');
+// console.table(cart.getItems());
+
+// console.log('Total: ', cart.countTotalPrice());
+
+/*
+ * =================================================
+ * ================== Finish Cart ==================
+ * =================================================
+ */
+
+// Работа с коллекцией (массивом объектов)
+// =================================================
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: false },
+//   { name: 'Ajax', online: false },
+// ];
+
+// console.table(friends);
+
+// Ищем друга по имени
+// =================================================
+// const findFriendByName = function (allFriends, friendName) {
+//   for (const friend of allFriends) {
+//     // console.log(friend);
+//     // console.log(friend.name);
+
+//     if (friend.name === friendName) {
+//       return friend;
+//     }
+//   }
+
+//   return 'НЕ НАШЛИ :(';
+// };
+
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
+
+// Получаем имена всех друзей
+// =================================================
+// const getAllNames = function (allFriends) {
+//   const names = [];
+
+//   for (const friend of allFriends) {
+//     console.log(friend.name);
+
+//     names.push(friend.name);
+//   }
+
+//   return names;
+// };
+
+// console.log(getAllNames(friends));
+
+// Получаем имена только друзей которые онлайн
+// =================================================
+// const getOnlineFriends = function (allFriends) {
+//   const onlineFriends = [];
+
+//   for (const friend of allFriends) {
+//     // console.log(friend);
+//     // console.log(friend.online);
+
+//     if (friend.online) {
+//       onlineFriends.push(friend);
+//     }
+//   }
+
+//   return onlineFriends;
+// };
+
+// console.log(getOnlineFriends(friends));
+
+// Получаем имена только друзей которые оффлайн
+// ==================================================
+// const getOfflineFriends = function (allFriends) {
+//   const offlineFriends = [];
+
+//   for (const friend of allFriends) {
+//     // console.log(friend.online);
+
+//     if (!friend.online) {
+//       offlineFriends.push(friend);
+//     }
+//   }
+
+//   return offlineFriends;
+// };
+
+// console.log(getOfflineFriends(friends));
+
+// Получаем обьект со статусом пользователей в сети
+// ==================================================
+// const getFriendsByStatus = function (allFriends) {
+//   const friendsByStatus = {
+//     online: [],
+//     offline: [],
+//   };
+
+//   for (const friend of allFriends) {
+//     if (friend.online) {
+//       friendsByStatus.online.push(friend);
+//       continue;
+//     }
+
+//     friendsByStatus.offline.push(friend);
+
+//     // const key = friend.online ? 'online' : 'offline';
+//     // friendsByStatus[key].push(friend);
+//   }
+
+//   return friendsByStatus;
+// };
+
+// console.log(getFriendsByStatus(friends));
+
+// =====================
+// Rysich ==============
+// =====================
